4d881cd368d56ea0314b6d9a7010350550224344
BenM4k <103831502+BenM4k@users.noreply.github.com>
2023-08-16 11:14:28 +0200

    update records

--- data.sql
+++ data.sql
@@ -11,3 +11,17 @@ INSERT INTO animals (id, name, date_of_birth, neutered, escape_attempts, weight_
 INSERT INTO animals (id, name, date_of_birth, neutered, escape_attempts, weight_kg) VALUES (3, 'Pikachu', '2021-1-7', false, 1, 15.04);
 
 INSERT INTO animals (id, name, date_of_birth, neutered, escape_attempts, weight_kg) VALUES (4, 'Devimon', '2017-5-12', true, 5, 11);
+
+INSERT INTO animals (id, name, date_of_birth, neutered, escape_attempts, weight_kg) VALUES (5, 'Charmander', '2020-2-8', false, 0, -11);
+
+INSERT INTO animals (id, name, date_of_birth, neutered, escape_attempts, weight_kg) VALUES (6, 'Plantmon', '2021-11-15', true, 2, -5.7);
+
+INSERT INTO animals (id, name, date_of_birth, neutered, escape_attempts, weight_kg) VALUES (7, 'Squirtle', '1993-2-4', false, 3, -12.13);
+
+INSERT INTO animals (id, name, date_of_birth, neutered, escape_attempts, weight_kg) VALUES (8, 'Angemon', '2005-6-12', true, 1, -45);
+
+INSERT INTO animals (id, name, date_of_birth, neutered, escape_attempts, weight_kg) VALUES (9, 'Boarmon', '2005-6-7', true, 7, 20.4);
+
+INSERT INTO animals (id, name, date_of_birth, neutered, escape_attempts, weight_kg) VALUES (10, 'Blossom', '1998-10-13', true, 3, 17);
+
+INSERT INTO animals (id, name, date_of_birth, neutered, escape_attempts, weight_kg) VALUES (11, 'Ditto', '2022-5-14', true, 4, 22);
